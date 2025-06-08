3dc3b0c673a45e8e03b36386693d8d6d53913a16
remorses <beats.by.morse@gmail.com>
2025-06-08 19:44:34 +0200

    remove some older themes

--- src/generate.ts
+++ src/generate.ts
@@ -14,14 +14,7 @@ const writeData: WriteData = {
   author: "Tommy D. Rossi & Pyae Sone Aung",
   themes: [
     getTheme({ themeKey: "light", name: "Spiceflow Light", type: "light" }),
-    getTheme({ themeKey: "light_colorblind", name: "Spiceflow Light High Contrast", type: "light" }),
     getTheme({ themeKey: "dark", name: "Spiceflow Dark", type: "dark" }),
-    getTheme({
-      themeKey: "dark_dimmed",
-      name: "Spiceflow Dark Dimmed",
-      type: "dark",
-    }),
-    getTheme({ themeKey: "dark_colorblind", name: "Spiceflow Dark High Contrast", type: "dark" }),
   ],
 };
 
